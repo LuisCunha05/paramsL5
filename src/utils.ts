8b0e8e523d3307ce839fa4a75d118dd32115bf40
LuisCunha05 <luisf.cunha05@gmail.com>
2025-08-31 12:50:38 -0300

    :sparkles: Feat(utils): Improved isNonEmptyString verification

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,3 @@
-
-
-export function isNonEmptyString(value: unknown):value is string {
-  return typeof value === "string" && value !== ''
+export function isNonEmptyString(value: unknown): value is string {
+	return typeof value === 'string' && value.trim().length > 0
 }
