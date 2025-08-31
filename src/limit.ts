003fe03e8019a9213933beb115f85110f924654c
LuisCunha05 <luisf.cunha05@gmail.com>
2025-08-31 13:07:10 -0300

    :recycle: refactor(limit): added isNumber to utils and change limit validation

--- src/limit.ts
+++ src/limit.ts
@@ -1,37 +1,35 @@
-import {IValueParams, AValidation} from '@/types'
+import { AValidation, type IValueParams } from '@/types'
+import { isNumber } from '@/utils'
 
 export type TLimit = number
 
 export class Limit extends AValidation implements IValueParams<TLimit> {
-    private state: TLimit = 10
+	private state: TLimit = 10
 
-    constructor(arg?: TLimit) {
-        super()
-        if(!this.isInputValid(arg))return
-        this.state = arg
-    }
+	constructor(arg?: TLimit) {
+		super()
+		if (!this.isInputValid(arg)) return
+		this.state = arg
+	}
 
-    public get(): TLimit {
-        return this.state
-    }
+	public get(): TLimit {
+		return this.state
+	}
 
-    public toParams(): string {
-        return `limit=${this.state}`
-    }
+	public toParams(): string {
+		return `limit=${this.state}`
+	}
 
-    public set(arg?: number) {
-        if(!this.isInputValid(arg))return
-        this.state = arg
-    }
+	public set(arg?: number) {
+		if (!this.isInputValid(arg)) return
+		this.state = arg
+	}
 
-    protected isInputValid(arg?: number): arg is number {
-        if(
-            typeof arg !== 'number' ||
-            Number.isNaN(arg) ||
-            !Number.isFinite(arg) ||
-            !Number.isInteger(arg)
-        )return false
+	protected isInputValid(arg?: number): arg is number {
+		const predicate = (value: number) => {
+			return Number.isInteger(value) && value > 0
+		}
 
-        return arg > 0
-    }
+		return isNumber(arg, predicate)
+	}
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,13 @@
 export function isNonEmptyString(value: unknown): value is string {
 	return typeof value === 'string' && value.trim().length > 0
 }
+
+export function isNumber(
+	arg: unknown,
+	predicate?: (value: number) => boolean,
+): arg is number {
+	if (typeof arg !== 'number' || Number.isNaN(arg) || !Number.isFinite(arg))
+		return false
+
+	return predicate ? predicate(arg) : true
+}
