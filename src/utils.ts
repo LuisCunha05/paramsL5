50c3ea023df96f5f5028780c71edb6b771e7e8c6
LuisCunha05 <luisf.cunha05@gmail.com>
2025-09-07 23:14:31 -0300

    :sparkles: feat(utils): Added isBaseValue

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import {BaseValue} from "@/types";
+
 export function isNonEmptyString(value: unknown): value is string {
 	return typeof value === 'string' && value.trim().length > 0
 }
@@ -10,6 +12,10 @@ export function isNumber(
 		return false
 
 	return predicate ? predicate(arg) : true
+}
+
+export function isBaseValue(arg: unknown): arg is BaseValue {
+	return !(typeof arg !== 'boolean' && !isNumber(arg) && !isNonEmptyString(arg));
 }
 
 export function typeName(arg?: unknown): string {
