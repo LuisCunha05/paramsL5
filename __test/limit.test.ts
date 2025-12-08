51b6ee67d818ab4a255be9e9704b38b19611ecb8
LuisCunha05 <luisf.cunha05@gmail.com>
2025-12-07 21:20:11 -0400

    :wrench: chore: format and changed tsconfig to inclide tests

--- __test/limit.test.ts
+++ __test/limit.test.ts
@@ -1,44 +1,43 @@
-import {Limit} from "@/limit";
+import { Limit } from "@/limit";
 
 describe("Limit", () => {
   let limit: Limit;
 
   beforeEach(() => {
     limit = new Limit();
-  })
+  });
 
   test("Should set limit to a parameter", () => {
     limit.set(7);
 
     expect(limit.get()).toBe(7);
-  })
+  });
 
   test("Should have a default value", () => {
     expect(limit.get()).toBe(10);
-  })
+  });
 
   test("Should error for non-number arguments", () => {
-
     expect(() => {
       //@ts-expect-error
       limit.set();
-    }).toThrow(`Limit must be a positive integer or zero, got undefined instead`)
+    }).toThrow(`Limit must be a positive integer or zero, got undefined instead`);
 
     expect(() => {
       //@ts-expect-error
       limit.set(null);
-    }).toThrow(`Limit must be a positive integer or zero, got null instead`)
+    }).toThrow(`Limit must be a positive integer or zero, got null instead`);
 
     expect(() => {
       //@ts-expect-error
       limit.set({});
-    }).toThrow(`Limit must be a positive integer or zero, got object instead`)
+    }).toThrow(`Limit must be a positive integer or zero, got object instead`);
 
     expect(() => {
       //@ts-expect-error
       limit.set([]);
-    }).toThrow(`Limit must be a positive integer or zero, got Array instead`)
-  })
+    }).toThrow(`Limit must be a positive integer or zero, got Array instead`);
+  });
 
   test("Should change limit to a parameter", () => {
     limit.set(7);
@@ -48,20 +47,19 @@ describe("Limit", () => {
     limit.set(10);
 
     expect(limit.get()).toBe(10);
-  })
+  });
 
   test("Should error for negative numbers", () => {
-    expect(() => limit.set(-10)).toThrow('Limit must be a positive integer or zero, got number instead')
-  })
+    expect(() => limit.set(-10)).toThrow("Limit must be a positive integer or zero, got number instead");
+  });
 
   test("Should generate correct params string for default value", () => {
-
     expect(limit.toParams()).toBe("limit=10");
-  })
+  });
 
   test("Should generate correct params string for non-default value", () => {
     limit.set(7);
 
     expect(limit.toParams()).toBe("limit=7");
-  })
-})
+  });
+});
