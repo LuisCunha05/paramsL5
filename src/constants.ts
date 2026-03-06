993aa4f8f6c2501ca26ad99c0af1f459b28c1f29
Luis Felipe Gomes Cunha <lcunha@grupoimagetech.com.br>
2026-03-06 10:30:51 -0400

    :sparkles: feat(constants): added object reference for url encoded special characters

---
+++ src/constants.ts
@@ -0,0 +1,52 @@
+export const URL_ENCODED_CHARS = {
+  /** Example: `,` */
+  COMMA: '%2C',
+  /** Example: `:` */
+  COLON: '%3A',
+  /** Example: `=` */
+  EQUALS: '%3D',
+  /** Example: `;` */
+  SEMICOLON: '%3B',
+  /** Example: `>` */
+  GREATER_THAN: '%3E',
+  /** Example: `<` */
+  LESS_THAN: '%3C',
+  /** Example: ` ` (Space) */
+  SPACE: '%20',
+  /** Example: `&` */
+  AMPERSAND: '%26',
+  /** Example: `#` */
+  HASHTAG: '%23',
+  /** Example: `?` */
+  QUESTION_MARK: '%3F',
+  /** Example: `/` */
+  SLASH: '%2F',
+  /** Example: `@` */
+  AT: '%40',
+  /** Example: `+` */
+  PLUS: '%2B',
+  /** Example: `$` */
+  DOLLAR: '%24',
+  /** Example: `%` */
+  PERCENT: '%25',
+  /** Example: `^` */
+  CARET: '%5E',
+  /** Example: `|` */
+  PIPE: '%7C',
+  /** Example: `~` */
+  TILDE: '%7E',
+  /** Example: '`' */
+  BACKTICK: '%60',
+  /** Example: `"` */
+  QUOTE: '%22',
+  /** Example: `'` */
+  APOSTROPHE: '%27',
+  /** Example: `[` */
+  LEFT_BRACKET: '%5B',
+  /** Example: `]` */
+  RIGHT_BRACKET: '%5D',
+  /** Example: `{` */
+  LEFT_BRACE: '%7B',
+  /** Example: `}` */
+  RIGHT_BRACE: '%7D',
+} as const
