ff6d661f0bc7e6d2a4cdd78227a94313a89cd8d0
Luis Felipe Gomes Cunha <lcunha@grupoimagetech.com.br>
2026-03-06 14:19:51 -0400

    :sparkles: feat(with): added functional implementation of with called withRel

---
+++ src/with.ts
@@ -0,0 +1,28 @@
+import { isNonEmptyString, typeName } from '@/utils'
+
+export type TWith = Array<string>
+
+export function withRel(arg: TWith) {
+  if (!Array.isArray(arg)) {
+    console.error(
+      `Argument of withRel must be a array, got ${typeName(arg)} instead`,
+    )
+    return
+  }
+
+  const filteredValues = arg.filter((item) => {
+    if (!isNonEmptyString(item)) {
+      console.error(
+        `withRel value must be a string, got ${typeName(arg)} instead`,
+      )
+      return false
+    }
+    return true
+  })
+  const uniqueValues = new Set(filteredValues)
+
+  const params = new URLSearchParams()
+  params.set('with', Array.from(uniqueValues).join(';'))
+
+  return params.toString()
+}
