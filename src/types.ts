c71ee1f32acf97ec5829669321a055f2d90a4e7b
Luis Felipe Gomes Cunha <lcunha@grupoimagetech.com.br>
2026-03-06 11:47:09 -0400

    :wrench: chore: removed types for classes not utilized

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,1 @@
-export abstract class BaseParam<T> {
-  protected abstract isInputValid(...arg: unknown[]): boolean
-  public abstract get(): T
-  public abstract toParams(): string
-}
-
-export abstract class CollectionParam<T> extends BaseParam<T> {
-  public abstract add(...args: unknown[]): void
-}
-
-export abstract class ValueParam<T> extends BaseParam<T> {
-  public abstract set(...args: unknown[]): void
-}
-
 export type BaseValue = string | number | boolean
