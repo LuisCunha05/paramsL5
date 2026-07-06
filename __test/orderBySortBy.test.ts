aac388be287b7d35ff3015a147af7588de6eb090
Luis Felipe Gomes Cunha <lcunha@grupoimagetech.com.br>
2026-07-06 17:59:13 -0400

    :recycle: refactor(tests): refactor orderBySortBy for new api

--- __test/orderBySortBy.test.ts
+++ __test/orderBySortBy.test.ts
@@ -4,8 +4,10 @@ import { orderBySortBy } from '@/generators/orderBySortBy'
 
 // biome-ignore lint/suspicious/noExplicitAny: Used to avoid many ts-expected-errors in the tests
 let input: any
-let result: string | undefined
-let expected: string | undefined
+// biome-ignore lint/suspicious/noExplicitAny: Used to avoid many ts-expected-errors in the tests
+let result: any
+// biome-ignore lint/suspicious/noExplicitAny: Used to avoid many ts-expected-errors in the tests
+let expected: any
 
 const consoleError = vi.spyOn(console, 'error').mockImplementation(() => {})
 
@@ -23,11 +25,14 @@ describe('orderBySortBy function', () => {
   describe('formatting', () => {
     test('should generate correct URL params for a single order/sort pair', () => {
       input = [['name', SORT_BY.ASC]]
-      expected = 'orderBy=name&sortedBy=asc'
+      expected = {
+        orderBy: { raw: 'name', encoded: 'name' },
+        sortedBy: { raw: 'asc', encoded: 'asc' },
+      }
 
       result = orderBySortBy(input)
 
-      expect(result).toBe(expected)
+      expect(result).toEqual(expected)
     })
 
     test('should generate correct string params with multiple order/sort pairs', () => {
@@ -36,11 +41,20 @@ describe('orderBySortBy function', () => {
         ['age', SORT_BY.DESC],
         ['created_at', SORT_BY.ASC],
       ]
-      expected = `orderBy=name${URC.SEMICOLON}age${URC.SEMICOLON}created_at&sortedBy=asc${URC.SEMICOLON}desc${URC.SEMICOLON}asc`
+      expected = {
+        orderBy: {
+          raw: 'name;age;created_at',
+          encoded: `name${URC.SEMICOLON}age${URC.SEMICOLON}created_at`,
+        },
+        sortedBy: {
+          raw: 'asc;desc;asc',
+          encoded: `asc${URC.SEMICOLON}desc${URC.SEMICOLON}asc`,
+        },
+      }
 
       result = orderBySortBy(input)
 
-      expect(result).toBe(expected)
+      expect(result).toEqual(expected)
     })
 
     test('should deduplicate keys and capture the last element with same key', () => {
@@ -48,60 +62,121 @@ describe('orderBySortBy function', () => {
         ['name', SORT_BY.ASC],
         ['name', SORT_BY.DESC],
       ]
-      expected = 'orderBy=name&sortedBy=desc'
+      expected = {
+        orderBy: { raw: 'name', encoded: 'name' },
+        sortedBy: { raw: 'desc', encoded: 'desc' },
+      }
 
       result = orderBySortBy(input)
 
-      expect(result).toBe(expected)
+      expect(result).toEqual(expected)
     })
 
     test('should allow multiple single value arrays', () => {
       input = [['key1'], ['key2']]
-      expected = `orderBy=key1${URC.SEMICOLON}key2&sortedBy=asc${URC.SEMICOLON}asc`
+      expected = {
+        orderBy: {
+          raw: 'key1;key2',
+          encoded: `key1${URC.SEMICOLON}key2`,
+        },
+        sortedBy: {
+          raw: 'asc;asc',
+          encoded: `asc${URC.SEMICOLON}asc`,
+        },
+      }
 
       result = orderBySortBy(input)
 
-      expect(result).toBe(expected)
+      expect(result).toEqual(expected)
     })
   })
 
   describe('default options', () => {
     test('should allow key-only value with default ASC sortBy', () => {
       input = [['key']]
-      expected = 'orderBy=key&sortedBy=asc'
+      expected = {
+        orderBy: { raw: 'key', encoded: 'key' },
+        sortedBy: { raw: 'asc', encoded: 'asc' },
+      }
 
       result = orderBySortBy(input)
 
-      expect(result).toBe(expected)
+      expect(result).toEqual(expected)
     })
 
     test('should change the default sortBy', () => {
       input = [['key']]
-      expected = 'orderBy=key&sortedBy=desc'
+      expected = {
+        orderBy: { raw: 'key', encoded: 'key' },
+        sortedBy: { raw: 'desc', encoded: 'desc' },
+      }
 
       result = orderBySortBy(input, { defaultSortBy: SORT_BY.DESC })
 
-      expect(result).toBe(expected)
+      expect(result).toEqual(expected)
     })
 
     test('should allow multiple single value arrays with the default sortBy', () => {
       input = [['key1'], ['key2']]
-      expected = `orderBy=key1${URC.SEMICOLON}key2&sortedBy=desc${URC.SEMICOLON}desc`
+      expected = {
+        orderBy: {
+          raw: 'key1;key2',
+          encoded: `key1${URC.SEMICOLON}key2`,
+        },
+        sortedBy: {
+          raw: 'desc;desc',
+          encoded: `desc${URC.SEMICOLON}desc`,
+        },
+      }
 
       result = orderBySortBy(input, { defaultSortBy: SORT_BY.DESC })
 
-      expect(result).toBe(expected)
+      expect(result).toEqual(expected)
     })
   })
 
   describe('validations and edge cases', () => {
-    test('should return undefined with empty input', () => {
-      expect(orderBySortBy()).toBeUndefined()
+    test('should return empty result with empty input', () => {
+      expected = {
+        orderBy: undefined,
+        sortedBy: undefined,
+      }
+
+      result = orderBySortBy()
+
+      expect(result).toEqual(expected)
     })
 
+    test('should return empty result if called with a non-array', () => {
+      input = {}
+      expected = {
+        orderBy: undefined,
+        sortedBy: undefined,
+      }
+
+      result = orderBySortBy(input)
+
+      expect(result).toEqual(expected)
+    })
+
+    test('should return empty result if all items are invalid', () => {
+      input = ['invalid']
+      expected = {
+        orderBy: undefined,
+        sortedBy: undefined,
+      }
+
+      result = orderBySortBy(input)
+
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe('OrderBySortBy logging', () => {
     test('should log error if arg is not an array', () => {
       input = {}
-      orderBySortBy(input)
+
+      orderBySortBy(input, { logger: console })
 
       expect(consoleError).toHaveBeenCalledWith(
         expect.stringContaining(
@@ -112,7 +187,8 @@ describe('orderBySortBy function', () => {
 
     test('should log error if an item is not an array', () => {
       input = ['invalid']
-      orderBySortBy(input)
+
+      orderBySortBy(input, { logger: console })
 
       expect(consoleError).toHaveBeenCalledWith(
         expect.stringContaining(
@@ -121,9 +197,10 @@ describe('orderBySortBy function', () => {
       )
     })
 
-    test('should log error if an item does not have exactly 0 elements', () => {
+    test('should log error if an item is empty', () => {
       input = [[]]
-      orderBySortBy(input)
+
+      orderBySortBy(input, { logger: console })
 
       expect(consoleError).toHaveBeenCalledWith(
         expect.stringContaining(
@@ -132,9 +209,10 @@ describe('orderBySortBy function', () => {
       )
     })
 
-    test('should log error if an item does not have exactly 3 elements', () => {
+    test('should log error if an item has too many elements', () => {
       input = [['alog', 'acs', 'bla']]
-      orderBySortBy(input)
+
+      orderBySortBy(input, { logger: console })
 
       expect(consoleError).toHaveBeenCalledWith(
         expect.stringContaining(
@@ -145,7 +223,8 @@ describe('orderBySortBy function', () => {
 
     test('should log error if key is not a non-empty string', () => {
       input = [[123, SORT_BY.ASC]]
-      orderBySortBy(input)
+
+      orderBySortBy(input, { logger: console })
 
       expect(consoleError).toHaveBeenCalledWith(
         expect.stringContaining(
@@ -156,7 +235,8 @@ describe('orderBySortBy function', () => {
 
     test('should log error if sort value is not a valid SORT_BY value', () => {
       input = [['name', 'invalid_sort']]
-      orderBySortBy(input)
+
+      orderBySortBy(input, { logger: console })
 
       expect(consoleError).toHaveBeenCalledWith(
         expect.stringContaining(
